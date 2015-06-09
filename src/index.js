4a402ab9ffc75b63844d3dbf342710ced9970ad4
Devin Pastoor <devin.pastoor@gmail.com>
2015-06-09 15:02:05 -0400

    yay working example

--- src/index.js
+++ src/index.js
@@ -1,26 +1,45 @@
 var React = require('react');
 var ReactD3 = require('react-d3');
+var _ = require('lodash');
+
+var concTimeArray = function(cl, v, dose, times, idname) {
+	var c0 = dose/v;
+	var res = _.map(times, function(t) {
+		return(
+			{'x': t,
+			'y': c0*Math.exp(-cl/v*t)}
+			)
+		});
+	return {values: res, name: idname};
+}
+var baseline = concTimeArray(10, 100, 1000, [0, 0.25, 0.5, 1, 2, 4, 6, 8, 12], "Reference");
 
 var App = React.createClass({
 	getInitialState: function() {
 		return {
-			cl: 1,
-			vd: 10
+			cl: 10,
+			vd: 100,
+            data: [baseline,
+            	concTimeArray(10, 100, 1000, [0, 0.25, 0.5, 1, 2, 4, 6, 8, 12], "New ID")]
 		}
 	},	
-	update: function(e) {
+	update: function() {
 		this.setState({
 			cl: this.refs.cl.refs.inp.getDOMNode().value,
 			vd: this.refs.vd.refs.inp.getDOMNode().value,
+			data: [baseline, concTimeArray( this.refs.cl.refs.inp.getDOMNode().value, 
+				this.refs.vd.refs.inp.getDOMNode().value,
+			 1000, [0, 0.25, 0.5, 1, 2, 4, 6, 8, 12],"New ID")]
 		});
 	},
 	render:function(){
 		return(
 			<div>
-			<Slider ref = "cl" update={this.update} />
-			<label>{this.state.cl}</label>
-			<Slider ref = "vd" update={this.update} />
-			<label>{this.state.vd}</label>
+			<Slider ref = "cl" min ="1" max="20" defaultVal="10" update={this.update} />
+			<label>Cl = {this.state.cl}</label>
+			<Slider ref = "vd"  min ="1" max="200" defaultVal="100" update={this.update} />
+			<label>Vd = {this.state.vd}</label>
+			<SomeComponent data={this.state.data} />
 			</div>
 			)
 		}
@@ -29,7 +48,7 @@ var App = React.createClass({
 var Slider = React.createClass({
 	render:function() {
 		return(
-			<input ref ="inp" type="range" min="1" max="255" onChange={this.props.update} />
+			<input ref ="inp" type="range" min={this.props.min} max= {this.props.max} defaultValue={this.props.defaultVal} onChange={this.props.update} />
 			);
 	}
 });
@@ -38,27 +57,20 @@ var Slider = React.createClass({
     var Brush = ReactD3.Brush;
 
 var SomeComponent = React.createClass({
-    getInitialState: function() {
-        return {
-            data:     {
-          values: [{x:0,y:0},{x:0.25,y:8.61},{x:0.5,y:19.44},{x:1,y:34.01},{x:2,y:30.23},{x:3,y:31.3},
-          {x:4,y:24.98},{x:6,y:23.38},{x:8,y:23.51},{x:12,y:14.68},{x:16,y:9.07},{x:24,y:5.3}] ,
-          name: "ID1"
-      	}
-       };
-    },
-
     render: function() {
         return (
+        		<div>
+        		<hr />
 			<LineChart
 			  legend={true}
-                   data={this.state.data}
+                   data={this.props.data}
                    width={800}
                    height={400}
                 />
+                </div>
         );
     }
 
 });
+
 React.render(<App />, document.getElementById('root'));
-React.render(<SomeComponent />, document.getElementById('Chart'));
